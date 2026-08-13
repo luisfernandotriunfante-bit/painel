8f46472d6aff7b022f58ebd498b514b2596bd59e
luisfernandotriunfante-bit <luis.fernandotriunfante@gmail.com>
2026-08-13 10:04:10 -0400

    feat: enable scroll-driven Triunfante background

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './AppShell'
+import App from './AppShellAnimated'
 import './styles.css'
 import './real-data.css'
 import './enhanced.css'
