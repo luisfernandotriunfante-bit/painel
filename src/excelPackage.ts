e5c08aa77572f8ee47cdcd303959428e3baf5c58
luisfernandotriunfante-bit <luis.fernandotriunfante@gmail.com>
2026-08-13 13:30:32 -0400

    fix: generate Excel package synchronously with fflate

--- src/excelPackage.ts
+++ src/excelPackage.ts
@@ -1,12 +1,12 @@
-import JSZip from 'jszip'
+import { strFromU8, strToU8, unzipSync, zipSync } from 'fflate'
 import { fillSheet1 } from './excelSheet1'
 import { fillTeam } from './excelSheetTeam'
 import { NS, parseXml, serializeXml } from './excelXmlCore'
 
-function requireFile(zip: JSZip, path: string) {
-  const file = zip.file(path)
-  if (!file) throw new Error(`O modelo Excel selecionado não contém o componente obrigatório: ${path}`)
-  return file
+function requireEntry(files: Record<string, Uint8Array>, path: string) {
+  const value = files[path]
+  if (!value) throw new Error(`O modelo Excel selecionado não contém o componente obrigatório: ${path}`)
+  return value
 }
 
 function forceRecalc(document: XMLDocument) {
@@ -20,34 +20,24 @@ function forceRecalc(document: XMLDocument) {
   calc.setAttribute('forceFullCalc', '1')
 }
 
-export async function buildExcel(data: ArrayBuffer, state: any) {
-  const zip = await JSZip.loadAsync(data)
+export function buildExcel(data: ArrayBuffer, state: any) {
+  const files = unzipSync(new Uint8Array(data))
   const sheet1Path = 'xl/worksheets/sheet1.xml'
   const sheet2Path = 'xl/worksheets/sheet2.xml'
   const workbookPath = 'xl/workbook.xml'
 
-  const [sheet1Xml, sheet2Xml, workbookXml] = await Promise.all([
-    requireFile(zip, sheet1Path).async('string'),
-    requireFile(zip, sheet2Path).async('string'),
-    requireFile(zip, workbookPath).async('string'),
-  ])
-
-  const sheet1 = parseXml(sheet1Xml)
-  const sheet2 = parseXml(sheet2Xml)
-  const workbook = parseXml(workbookXml)
+  const sheet1 = parseXml(strFromU8(requireEntry(files, sheet1Path)))
+  const sheet2 = parseXml(strFromU8(requireEntry(files, sheet2Path)))
+  const workbook = parseXml(strFromU8(requireEntry(files, workbookPath)))
 
   const timing = fillSheet1(sheet1, state)
   fillTeam(sheet2, state, timing.worked, timing.targetDays)
   forceRecalc(workbook)
 
-  zip.file(sheet1Path, serializeXml(sheet1))
-  zip.file(sheet2Path, serializeXml(sheet2))
-  zip.file(workbookPath, serializeXml(workbook))
-  zip.remove('xl/calcChain.xml')
+  files[sheet1Path] = strToU8(serializeXml(sheet1))
+  files[sheet2Path] = strToU8(serializeXml(sheet2))
+  files[workbookPath] = strToU8(serializeXml(workbook))
+  delete files['xl/calcChain.xml']
 
-  return zip.generateAsync({
-    type: 'uint8array',
-    compression: 'DEFLATE',
-    compressionOptions: { level: 6 },
-  })
+  return zipSync(files, { level: 6 })
 }
