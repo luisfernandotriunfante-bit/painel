5a02cb20f06e7c39116777f6015441e4a1fc22ec
luisfernandotriunfante-bit <luis.fernandotriunfante@gmail.com>
2026-08-12 23:21:34 -0400

    feat: enable premium module sidebar shell

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './AppV3'
+import App from './AppShell'
 import './styles.css'
 import './real-data.css'
 import './enhanced.css'
 import './enhanced-v3.css'
+import './premium-shell.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
